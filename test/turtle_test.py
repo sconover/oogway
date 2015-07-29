bbaa8e5be007d5dff8de8abe2927e71f2729e962
Steve Conover <steve@squareup.com>
2015-07-28 21:50:37 -0700

    first working code with v2 block api

--- test/turtle_test.py
+++ test/turtle_test.py
@@ -10,12 +10,16 @@ sys.path.append(turtle_path)
 
 import unittest
 import mcpi.minecraft
+from mcpi.gamedata import block
 import turtle
 
 class TestTurtle(unittest.TestCase):
   def test_basic(self):
     mcpi_minecraft = mcpi.minecraft.Minecraft.create()
-    mcpi_minecraft.setBlock(1, 110, 1, mcpi.block.GOLD_BLOCK.id)
+    mcpi_minecraft.setBlockV2(1, 120, 1, block.STONE.name, variant=block.STONE.VARIANT_ANDESITE.value)
+
+# pen_down(blocks.SAND, blocks.SAND.VARIANT_RED_SAND)
+# pen_up()
 
 if __name__ == '__main__':
     unittest.main()

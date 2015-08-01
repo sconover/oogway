52d1f0e256adc0d64cd85319f5268f627bb2d96b
Steve Conover <steve@squareup.com>
2015-08-01 14:37:18 -0700

    the whole compass, ugly

--- oogway/raster.py
+++ oogway/raster.py
@@ -1,38 +1,78 @@
 import math
 
-    # # advance in the y-only direction one step, then advance once diagonally (+x and +y). repeat.
-
-    # angle = 23
-
-    # absolute_x_pos = 100
-    # absolute_y_pos = 100
-    # self.assertEqual((0, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
-
-    # absolute_x_pos = 100
-    # absolute_y_pos = 101
-    # self.assertEqual((1, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
-
-    # absolute_x_pos = 101
-    # absolute_y_pos = 102
-    # self.assertEqual((0, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
-
-    # absolute_x_pos = 101
-    # absolute_y_pos = 103
-    # self.assertEqual((1, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
-
 def calculate_increment_2d(absolute_position, angle_in_degrees, move_distance=1):
-    absolute_x = absolute_position[0]
-    absolute_y = absolute_position[1]
+  absolute_x = absolute_position[0]
+  absolute_y = absolute_position[1]
 
+  if angle_in_degrees <= 45:
     adjacent = move_distance
     opposite = math.tan(math.radians(angle_in_degrees)) / adjacent
-
-    x_diff = 0
-    y_diff = move_distance
-
     total_steps_before_increment = int(move_distance / opposite)
-    if absolute_y % total_steps_before_increment == total_steps_before_increment - 1:
-        return (move_distance, move_distance)
-    else:
-        return (0, move_distance)
 
+    if absolute_y % total_steps_before_increment == total_steps_before_increment - 1:
+      return (move_distance, move_distance)
+    else:
+      return (0, move_distance)
+  elif angle_in_degrees <= 90:
+    adjacent = move_distance
+    opposite = math.tan(math.radians(90 - angle_in_degrees)) / adjacent
+    total_steps_before_increment = int(move_distance / opposite)
+
+    if absolute_x % total_steps_before_increment == total_steps_before_increment - 1:
+      return (move_distance, move_distance)
+    else:
+      return (move_distance, 0)
+  elif angle_in_degrees <= 135:
+    adjacent = move_distance
+    opposite = math.tan(math.radians(angle_in_degrees - 90)) / adjacent
+    total_steps_before_increment = int(move_distance / opposite)
+
+    if absolute_x % total_steps_before_increment == total_steps_before_increment - 1:
+      return (move_distance, -1 * move_distance)
+    else:
+      return (move_distance, 0)
+  elif angle_in_degrees <= 180:
+    adjacent = move_distance
+    opposite = math.tan(math.radians(180 - angle_in_degrees)) / adjacent
+    total_steps_before_increment = int(move_distance / opposite)
+
+    if absolute_y % total_steps_before_increment == total_steps_before_increment - 1:
+      return (move_distance, -1 * move_distance)
+    else:
+      return (0, -1 * move_distance)
+  elif angle_in_degrees <= 225:
+    adjacent = move_distance
+    opposite = math.tan(math.radians(angle_in_degrees - 180)) / adjacent
+    total_steps_before_increment = int(move_distance / opposite)
+
+    if absolute_y % total_steps_before_increment == total_steps_before_increment - 1:
+      return (-1 * move_distance, -1 * move_distance)
+    else:
+      return (0, -1 * move_distance)
+  elif angle_in_degrees <= 270:
+    adjacent = move_distance
+    opposite = math.tan(math.radians(270 - angle_in_degrees)) / adjacent
+    total_steps_before_increment = int(move_distance / opposite)
+
+    if absolute_x % total_steps_before_increment == total_steps_before_increment - 1:
+      return (-1 * move_distance, -1 * move_distance)
+    else:
+      return (-1 * move_distance, 0)
+  elif angle_in_degrees <= 315:
+    adjacent = move_distance
+    opposite = math.tan(math.radians(angle_in_degrees - 270)) / adjacent
+    total_steps_before_increment = int(move_distance / opposite)
+
+    if absolute_x % total_steps_before_increment == total_steps_before_increment - 1:
+      return (-1 * move_distance, move_distance)
+    else:
+      return (-1 * move_distance, 0)
+  elif angle_in_degrees <= 360:
+    adjacent = move_distance
+    opposite = math.tan(math.radians(360 - angle_in_degrees)) / adjacent
+    total_steps_before_increment = int(move_distance / opposite)
+
+    if absolute_y % total_steps_before_increment == total_steps_before_increment - 1:
+      return (-1 * move_distance, move_distance)
+    else:
+      return (0, move_distance)
--- test/raster_test.py
+++ test/raster_test.py
@@ -70,6 +70,154 @@ class TestRaster(unittest.TestCase):
     absolute_y_pos = 104
     self.assertEqual((0, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
 
+  def test_calculate_diffs_angle_68(self):
+    # advance in the x-only direction one step, then advance once diagonally (+x and +y). repeat.
+
+    angle = 68
+
+    absolute_x_pos = 100
+    absolute_y_pos = 100
+    self.assertEqual((1, 0), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 101
+    absolute_y_pos = 100
+    self.assertEqual((1, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 102
+    absolute_y_pos = 101
+    self.assertEqual((1, 0), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 103
+    absolute_y_pos = 101
+    self.assertEqual((1, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+  def test_calculate_diffs_angle_68(self):
+    # advance in the x-only direction one step, then advance once diagonally (+x and -y). repeat.
+
+    angle = 113
+
+    absolute_x_pos = 100
+    absolute_y_pos = 100
+    self.assertEqual((1, 0), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 101
+    absolute_y_pos = 100
+    self.assertEqual((1, -1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 102
+    absolute_y_pos = 99
+    self.assertEqual((1, 0), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 103
+    absolute_y_pos = 99
+    self.assertEqual((1, -1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+  def test_calculate_diffs_angle_158(self):
+    # advance in the negative y-only direction one step, then advance once diagonally (+x and -y). repeat.
+
+    angle = 158
+
+    absolute_x_pos = 100
+    absolute_y_pos = 100
+    self.assertEqual((0, -1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 100
+    absolute_y_pos = 99
+    self.assertEqual((1, -1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 101
+    absolute_y_pos = 98
+    self.assertEqual((0, -1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 101
+    absolute_y_pos = 97
+    self.assertEqual((1, -1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+  def test_calculate_diffs_angle_203(self):
+    # advance in the negative y-only direction one step, then advance once diagonally (-x and -y). repeat.
+
+    angle = 203
+
+    absolute_x_pos = 100
+    absolute_y_pos = 100
+    self.assertEqual((0, -1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 100
+    absolute_y_pos = 99
+    self.assertEqual((-1, -1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 99
+    absolute_y_pos = 98
+    self.assertEqual((0, -1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 99
+    absolute_y_pos = 97
+    self.assertEqual((-1, -1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+  def test_calculate_diffs_angle_248(self):
+    # advance in the negative x-only direction one step, then advance once diagonally (-x and -y). repeat.
+
+    angle = 248
+
+    absolute_x_pos = 100
+    absolute_y_pos = 100
+    self.assertEqual((-1, 0), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 99
+    absolute_y_pos = 100
+    self.assertEqual((-1, -1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 98
+    absolute_y_pos = 99
+    self.assertEqual((-1, 0), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 97
+    absolute_y_pos = 99
+    self.assertEqual((-1, -1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+  def test_calculate_diffs_angle_293(self):
+    # advance in the negative x-only direction one step, then advance once diagonally (-x and +y). repeat.
+
+    angle = 293
+
+    absolute_x_pos = 100
+    absolute_y_pos = 100
+    self.assertEqual((-1, 0), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 99
+    absolute_y_pos = 100
+    self.assertEqual((-1, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 98
+    absolute_y_pos = 101
+    self.assertEqual((-1, 0), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 97
+    absolute_y_pos = 101
+    self.assertEqual((-1, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+  def test_calculate_diffs_angle_336(self):
+    # advance in the y-only direction one step, then advance once diagonally (-x and +y). repeat.
+
+    angle = 336
+
+    absolute_x_pos = 100
+    absolute_y_pos = 100
+    self.assertEqual((0, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 100
+    absolute_y_pos = 101
+    self.assertEqual((-1, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 99
+    absolute_y_pos = 102
+    self.assertEqual((0, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+    absolute_x_pos = 99
+    absolute_y_pos = 103
+    self.assertEqual((-1, 1), calculate_increment_2d((absolute_x_pos, absolute_y_pos), angle))
+
+
 
 if __name__ == '__main__':
     unittest.main()
